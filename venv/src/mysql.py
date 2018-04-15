4aa6f4ae887052f2fb290f16c9350eee225824b5
zhuhui901222 <15812902593@139.com>
2018-04-15 19:08:49 +0800

    第8次提

---
+++ venv/src/mysql.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import MySQLdb
+
+db = MySQLdb.connect("localhost", "root", "root", "test", charset='utf8' )
--- venv/www/index.py
+++ venv/www/index.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
+
+def application(environ, start_response):
+	# 创建 FieldStorage 的实例化
+	#print 124
+    status = '200 OK'
+    output = 'Hello World!'
+
+    response_headers = [('Content-type', 'text/plain'),
+                        ('Content-Length', str(len(output)))]
+    start_response(status, response_headers)
+ 
+    return [output]
+
+"""
 print "Content-type:text/html"
 print                               # 空行，告诉服务器结束头部
 print '<html>'
@@ -12,3 +26,4 @@ print '<body>'
 print '<h2>Hello World! 我是来自菜鸟教程的第一CGI程序</h2>'
 print '</body>'
 print '</html>'
+"""
