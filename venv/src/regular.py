3f069df276223d55564c25241ab64f7c1bcfa0e1
zhuhui901222 <15812902593@139.com>
2018-04-15 13:39:46 +0800

    第7次提

---
+++ venv/src/regular.py
@@ -0,0 +1,284 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+#123
+"""
+Python 正则表达式
+正则表达式是一个特殊的字符序列，它能帮助你方便的检查一个字符串是否与某种模式匹配。
+Python 自1.5版本起增加了re 模块，它提供 Perl 风格的正则表达式模式。
+re 模块使 Python 语言拥有全部的正则表达式功能。
+compile 函数根据一个模式字符串和可选的标志参数生成一个正则表达式对象。该对象拥有一系列方法用于正则表达式匹配和替换。
+re 模块也提供了与这些方法功能完全一致的函数，这些函数使用一个模式字符串做为它们的第一个参数。
+本章节主要介绍Python中常用的正则表达式处理函数。
+
+"""
+
+
+"""
+re.match函数
+re.match 尝试从字符串的起始位置匹配一个模式，如果不是起始位置匹配成功的话，match()就返回none。
+函数语法：
+re.match(pattern, string, flags=0)
+函数参数说明：
+参数	描述
+pattern	匹配的正则表达式
+string	要匹配的字符串。
+flags	标志位，用于控制正则表达式的匹配方式，如：是否区分大小写，多行匹配等等。参见：正则表达式修饰符 - 可选标志
+"""
+import re
+print(re.match('www', 'www.runoob.com').span())  # 在起始位置匹配
+print(re.match('com', 'www.runoob.com'))         # 不在起始位置匹配
+
+line = "Cats are smarter than dogs"
+
+matchObj = re.match( r'(.*) are (.*?) .*', line, re.M|re.I)
+
+if matchObj:
+   print "matchObj.group() : ", matchObj.group()
+   print "matchObj.group(1) : ", matchObj.group(1)
+   print "matchObj.group(2) : ", matchObj.group(2)
+else:
+   print "No match!!"
+
+
+
+"""
+re.search方法
+re.search 扫描整个字符串并返回第一个成功的匹配。
+函数语法：
+re.search(pattern, string, flags=0)
+函数参数说明：
+参数	描述
+pattern	匹配的正则表达式
+string	要匹配的字符串。
+flags	标志位，用于控制正则表达式的匹配方式，如：是否区分大小写，多行匹配等等。
+匹配成功re.search方法返回一个匹配的对象，否则返回None。
+我们可以使用group(num) 或 groups() 匹配对象函数来获取匹配表达式。
+匹配对象方法	描述
+group(num=0)	匹配的整个表达式的字符串，group() 可以一次输入多个组号，在这种情况下它将返回一个包含那些组所对应值的元组。
+groups()	返回一个包含所有小组字符串的元组，从 1 到 所含的小组号。
+"""
+import re
+print  "##"
+print re.search('www', 'www.runoob.com').span()  # 在起始位置匹配
+print re.search('com', 'www.runoob.com').span()         # 不在起始位置匹配
+
+
+line = "Cats are smarter than dogs";
+
+searchObj = re.search( r'(.*) are (.*?) .*', line, re.M|re.I)
+
+if searchObj:
+   print "searchObj.group() : ", searchObj.group()
+   print "searchObj.group(1) : ", searchObj.group(1)
+   print "searchObj.group(2) : ", searchObj.group(2)
+else:
+   print "Nothing found!!"
+
+
+#re.match与re.search的区别
+#re.match只匹配字符串的开始，如果字符串开始不符合正则表达式，则匹配失败，函数返回None；而re.search匹配整个字符串，直到找到一个匹配。
+
+print  "##"
+line = "Cats are smarter than dogs";
+
+matchObj = re.match( r'dogs', line, re.M|re.I)
+if matchObj:
+   print "match --> matchObj.group() : ", matchObj.group()
+else:
+   print "No match!!"
+
+matchObj = re.search( r'dogs', line, re.M|re.I)
+if matchObj:
+   print "search --> matchObj.group() : ", matchObj.group()
+else:
+   print "No match!!"
+
+
+"""
+检索和替换
+Python 的 re 模块提供了re.sub用于替换字符串中的匹配项。
+语法：
+re.sub(pattern, repl, string, count=0, flags=0)
+参数：
+pattern : 正则中的模式字符串。
+repl : 替换的字符串，也可为一个函数。
+string : 要被查找替换的原始字符串。
+count : 模式匹配后替换的最大次数，默认 0 表示替换所有的匹配。
+"""
+
+phone = "2004-959-559 # 这是一个国外电话号码"
+
+# 删除字符串中的 Python注释
+num = re.sub(r'#.*$', "", phone)
+print "电话号码是: ", num
+
+# 删除非数字(-)的字符串
+num = re.sub(r'\D', "", phone)
+print "电话号码是 : ", num
+
+
+"""
+检索和替换
+Python 的 re 模块提供了re.sub用于替换字符串中的匹配项。
+语法：
+re.sub(pattern, repl, string, count=0, flags=0)
+参数：
+pattern : 正则中的模式字符串。
+repl : 替换的字符串，也可为一个函数。
+string : 要被查找替换的原始字符串。
+count : 模式匹配后替换的最大次数，默认 0 表示替换所有的匹配。
+"""
+
+phone = "2004-959-559 # 这是一个国外电话号码"
+
+# 删除字符串中的 Python注释
+num = re.sub(r'#.*$', "", phone)
+print "电话号码是: ", num
+
+# 删除非数字(-)的字符串
+num = re.sub(r'\D', "", phone)
+print "电话号码是 : ", num
+
+# 将匹配的数字乘以 2
+def double(matched):
+    value = int(matched.group('value'))
+    return str(value * 2)
+
+s = 'A23G4HFD567'
+print(re.sub('(?P<value>\d+)', double, s))
+
+
+"""
+re.compile 函数
+compile 函数用于编译正则表达式，生成一个正则表达式（ Pattern ）对象，供 match() 和 search() 这两个函数使用。
+语法格式为：
+re.compile(pattern[, flags])
+参数：
+pattern : 一个字符串形式的正则表达式
+flags : 可选，表示匹配模式，比如忽略大小写，多行模式等，具体参数为：
+re.I 忽略大小写
+re.L 表示特殊字符集 \w, \W, \b, \B, \s, \S 依赖于当前环境
+re.M 多行模式
+re.S 即为 . 并且包括换行符在内的任意字符（. 不包括换行符）
+re.U 表示特殊字符集 \w, \W, \b, \B, \d, \D, \s, \S 依赖于 Unicode 字符属性数据库
+re.X 为了增加可读性，忽略空格和 # 后面的注释
+"""
+
+pattern = re.compile(r'\d+')                    # 用于匹配至少一个数字
+m = pattern.match('one12twothree34four')        # 查找头部，没有匹配
+print m
+
+m = pattern.match('one12twothree34four', 2, 10) # 从'e'的位置开始匹配，没有匹配
+print m
+
+m = pattern.match('one12twothree34four', 3, 10) # 从'1'的位置开始匹配，正好匹配
+print m
+
+print m.group(0)   # 可省略 0
+print m.start(0)   # 可省略 0
+print m.end(0)     # 可省略 0
+print m.span(0)    # 可省略 0
+
+pattern = re.compile(r'([a-z]+) ([a-z]+)', re.I)   # re.I 表示忽略大小写
+m = pattern.match('Hello World Wide Web')
+print m                               # 匹配成功，返回一个 Match 对象
+print m.group(0)                            # 返回匹配成功的整个子串
+print m.span(0)                             # 返回匹配成功的整个子串的索引
+print m.group(1)                            # 返回第一个分组匹配成功的子串
+print m.span(1)                             # 返回第一个分组匹配成功的子串的索引
+print m.group(2)                            # 返回第二个分组匹配成功的子串
+print m.span(2)                             # 返回第二个分组匹配成功的子串
+print m.groups()                            # 等价于 (m.group(1), m.group(2), ...)
+#print  m.group(3)                            # 不存在第三个分组  代码终止
+
+"""
+findall
+在字符串中找到正则表达式所匹配的所有子串，并返回一个列表，如果没有找到匹配的，则返回空列表。
+注意： match 和 search 是匹配一次 findall 匹配所有。
+语法格式为：
+findall(string[, pos[, endpos]])
+参数：
+string : 待匹配的字符串。
+pos : 可选参数，指定字符串的起始位置，默认为 0。
+endpos : 可选参数，指定字符串的结束位置，默认为字符串的长度。
+"""
+
+pattern = re.compile(r'\d+')   # 查找数字
+result1 = pattern.findall('runoob 123 google 456')
+result2 = pattern.findall('run88oob123google456', 0, 10)
+
+print(result1)
+print(result2)
+
+"""
+re.finditer
+和 findall 类似，在字符串中找到正则表达式所匹配的所有子串，并把它们作为一个迭代器返回。
+re.finditer(pattern, string, flags=0)
+参数：
+参数	描述
+pattern	匹配的正则表达式
+string	要匹配的字符串。
+flags	标志位，用于控制正则表达式的匹配方式，如：是否区分大小写，多行匹配等等。参见：正则表达式修饰符 - 可选标志
+"""
+
+it = re.finditer(r"\d+","12a32bc43jf3")
+for match in it:
+    print (match.group() )
+
+
+
+
+"""
+re.split
+split 方法按照能够匹配的子串将字符串分割后返回列表，它的使用形式如下：
+re.split(pattern, string[, maxsplit=0, flags=0])
+
+参数	描述
+pattern	匹配的正则表达式
+string	要匹配的字符串。
+maxsplit	分隔次数，maxsplit=1 分隔一次，默认为 0，不限制次数。
+flags	标志位，用于控制正则表达式的匹配方式，如：是否区分大小写，多行匹配等等。参见：正则表达式修饰符 - 可选标志
+"""
+
+print re.split('\W+', 'runoob, runoob, runoob.')
+print re.split('(\W+)', ' runoob, runoob, runoob.')
+print re.split('\W+', ' runoob, runoob, runoob.', 1)
+print re.split('a*', 'hello world')   # 对于一个找不到匹配的字符串而言，split 不会对其作出分割
+
+
+
+"""
+正则表达式修饰符 - 可选标志
+正则表达式可以包含一些可选标志修饰符来控制匹配的模式。修饰符被指定为一个可选的标志。多个标志可以通过按位 OR(|) 它们来指定。如 re.I | re.M 被设置成 I 和 M 标志：
+修饰符	描述
+re.I	使匹配对大小写不敏感
+re.L	做本地化识别（locale-aware）匹配
+re.M	多行匹配，影响 ^ 和 $
+re.S	使 . 匹配包括换行在内的所有字符
+re.U	根据Unicode字符集解析字符。这个标志影响 \w, \W, \b, \B.
+re.X	该标志通过给予你更灵活的格式以便你将正则表达式写得更易于理解。
+"""
+
+line = "Cats are smarter than dogs"
+matchObj = re.match( r'(.*) are (.*?) .*', line, re.M|re.I)
+if matchObj:
+    print "matchObj.group() : ", matchObj.group()
+    print "matchObj.group(1) : ", matchObj.group(1)
+    print "matchObj.group(2) : ", matchObj.group(2)
+else:
+    print "No match!!"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
